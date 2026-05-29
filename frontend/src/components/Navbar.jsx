da4f7fc2786163c88bb5427aec8f9855bb90e854
chimataraghuram <chimataraghuram@users.noreply.github.com>
2026-05-29 20:00:47 +0530

    style: increase logo icon size to 54px and decrease title text size to 0.92rem

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -110,7 +110,7 @@ const Navbar = ({ onChatToggle, onSearch, searchTerm }) => {
                         {isMenuOpen ? <X size={22} /> : <Menu size={22} />}
                     </button>
                     <a href="/" className="logo-container">
-                        <img src={logo} alt="TECHBOY STORE" className="logo-img" style={{ width: 42, height: 42 }} />
+                        <img src={logo} alt="TECHBOY STORE" className="logo-img" style={{ width: 54, height: 54 }} />
                         <span className="logo-text jelly-text">TECHBOY STORE</span>
                     </a>
                 </div>
